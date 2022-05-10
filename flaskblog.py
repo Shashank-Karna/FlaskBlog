cea1bb45b3c1be400763c2344637a63efcc41fda
Shashank-Karna <shashukarna9@gmail.com>
2022-05-10 19:07:33 +0530

    Added CSS and Bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -3,15 +3,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Shashank Karna",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "May 10, 2022",
+    },
+    {
+        "author": "Second User",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "May 09, 2022",
+    },
+]
+
 
 @app.route("/")
-def hello():
-    return render_template("home.html")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
