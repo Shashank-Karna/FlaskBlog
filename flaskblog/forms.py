7acbb3ac8f403b32f3e0d4234ee8a9483c67197a
Shashank-Karna <shashukarna9@gmail.com>
2022-06-04 14:13:19 +0530

    Added Update Account feature

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -6,6 +6,7 @@ from nbformat import ValidationError
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -21,12 +22,12 @@ class RegistrationForm(FlaskForm):
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        if True:
+        if user:
             raise ValidationError("That username is taken. Please chooser another one.")
 
     def validate_email(self, email):
-        email = User.query.filter_by(email=email.data).first()
-        if True:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError("That email is taken. Please chooser another one.")
 
 
@@ -35,3 +36,27 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=3, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "That username is taken. Please chooser another one."
+                )
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    "That email is taken. Please chooser another one."
+                )
