b0dc61770b8640909767a680580271634148cbe5
Shashank-Karna <shashukarna9@gmail.com>
2022-06-05 19:50:07 +0530

    Added Create Post feature

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,8 +3,9 @@ import imp
 from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
+from matplotlib.pyplot import title
 from nbformat import ValidationError
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -64,3 +65,9 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError(
                     "That email is taken. Please chooser another one."
                 )
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Post")
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 from click import password_option
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -110,3 +110,13 @@ def account():
     return render_template(
         "account.html", title="Account", image_file=image_file, form=form
     )
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash("Your post has been created!", "success")
+        return redirect(url_for("home"))
+    return render_template("create_post.html", title="New Post", form=form)
